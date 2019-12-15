6ec7c8aa9a4a41e99d3dd0f30c96c37a0a837fad
Jiying Cao <jiyingcao@yahoo.com>
2019-12-15 19:59:38 +0800

    Initial commit

---
+++ app/src/main/java/com/example/androidart/SecondActivity.java
@@ -0,0 +1,22 @@
+package com.example.androidart;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(v.getContext(), ThirdActivity.class));
+            }
+        });
+    }
+}
